9ef2fcd91ac70cfc3a48b64dd18b3e767236cffe
zaynzzz <89463457+zaynzzz@users.noreply.github.com>
2022-04-27 14:48:53 +0700

    Add files via upload

---
+++ editdata.php
@@ -0,0 +1,8 @@
+<h2> <?= $member->member_name; ?></h2>
+<div class="mb-3">
+    <label for="" class="form-label"></label>
+    <input type="text" class="form-control" name="" value="<?= $member->member_name; ?>" readonly id="" aria-describedby="helpId" placeholder="">
+    <input type="text" class="form-control" name="" value="<?= $member->member_email; ?>" id="" aria-describedby="helpId" placeholder="">
+    <input type="text" class="form-control" name="" value="<?= $member->member_telephone; ?>" id="" aria-describedby="helpId" placeholder="">
+    <input type="text" class="form-control" name="" value="<?= $member->discount; ?>" id="" aria-describedby="helpId" placeholder="">
+</div>
