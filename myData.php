bb9531a5fd625338dfae79616b5313a7c4e4c1b1
zaynzzz <89463457+zaynzzz@users.noreply.github.com>
2022-04-27 14:49:16 +0700

    Update myData.php

--- myData.php
+++ myData.php
@@ -14,6 +14,13 @@ class myData extends BaseController
         $data['member'] = $sql;
         return view('index', $data);
     }
+    public function editData()
+    {
+        $id = $this->request->getVar('id');
+        $sql = db_connect()->query("SELECT * FROM members WHERE member_id='$id'")->getRow();
+        $data['member'] = $sql;
+        return view("editdata", $data);
+    }
     public function getData()
     {
         $id = $this->request->getVar('id');
