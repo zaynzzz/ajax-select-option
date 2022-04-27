1b62bad83065ebac3d0bcd716ae5113bd552c6cb
zaynzzz <89463457+zaynzzz@users.noreply.github.com>
2022-04-27 14:48:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,11 +12,34 @@
 		<button type="submit" name='klikselect' onclick="getid(this.value)" value='<?= $member['member_id']; ?>' class="btn btn-primary"><?= $member['member_name']; ?></button>
 	<?php endforeach; ?>
 
+
+
 <body class='container my-5'>
+
+	<button name="" id="btnmodal" class="btn btn-primary|secondary|success|danger|warning|info|light|dark|link" href="#" role="button">Edit</button>
+	<div class="modal " id="editdata" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="editdata" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="staticBackdropLabel">Edit Data <?= $member['member_name']; ?></h5>
+					<button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div id="tampilkandata"></div>
+
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+
 	<div class="mb-3">
 		<label for="" class="form-label"></label>
 	</div>
-	<div id="tampilkandata"></div>
+	<!-- <div id="tampilkandata"></div> -->
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.min.js" integrity="sha512-OvBgP9A2JBgiRad/mM36mkzXSXaJE9BEIENnVEmeZdITvwT09xnxLtT4twkCa8m/loMbPHsvPl0T8lRGVBwjlQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -25,7 +48,7 @@
 		$('#gantiselect').change(function() {
 			var id = $(this).val();
 			$.ajax({
-				url: "<?php echo site_url('Home/getData'); ?>",
+				url: "<?php echo site_url('myData/getData'); ?>",
 				method: "POST",
 				data: {
 					id: id
@@ -44,9 +67,10 @@
 <script>
 	function getid(id) {
 		var id = id;
+		$("#editdata").toggle();
 		$.ajax({
 			type: "POST",
-			url: "<?= base_url("Home/getData"); ?>",
+			url: "<?= base_url("myData/editData"); ?>",
 			data: {
 				"id": id
 			},
@@ -58,6 +82,14 @@
 			}
 		});
 	}
+</script>
+<script>
+	$(document).ready(function() {
+		$("#btnmodal").click(function(e) {
+			e.preventDefault();
+			$("#editdata").toggle();
+		});
+	});
 </script>
 
 </html>
